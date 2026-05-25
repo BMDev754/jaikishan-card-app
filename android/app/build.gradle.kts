3deaae081654c1a03486051b0911b8879c494d5d
acbright <acbrightsymon@gmail.com>
2026-05-25 15:57:04 +0530

    fix the 16 kb problem

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,17 @@ if (keyPropertiesFile.exists()) {
 
 android {
     namespace = "com.jaykisan.jaykisan_card"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36
+    ndkVersion = "28.0.12433566"
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+    
+    aaptOptions {
+        noCompress += listOf("lib")
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -35,7 +44,7 @@ android {
         applicationId = "com.jaykisan.jaykisan_card"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
